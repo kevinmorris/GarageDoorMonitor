041c5a51aaec3aa7b9457fffc2cc01cb4deecf69
Kevin Morris <kmorris511@gmail.com>
2023-09-04 17:42:14 -0600

    posting garage door data

--- GarageDoorMonitorArduino/src/main.cpp
+++ GarageDoorMonitorArduino/src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
-#include <SPI.h>
-#include <WiFi.h>
+#include <WiFiS3.h>
 #include "app_settings.h"
 
 const char* networkName = WIFI_NETWORK_NAME;
 const char* networkPassword = WIFI_PASSWORD;
 int status = WL_IDLE_STATUS;
+
+WiFiClient client;
 
 float voltage = 0.0f;
 
@@ -18,7 +19,6 @@ void setup() {
     while(!Serial);
 
     Serial.println("Starting...");
-    // check for the WiFi module:
     if (WiFi.status() == WL_NO_MODULE) {
         Serial.println("Communication with WiFi module failed");
         while (true);
@@ -39,7 +39,7 @@ void setup() {
 
 void loop() {
 
-    float currentVoltage = voltage > VOLTAGE_THRESHOLD ? 0.0f : 2.5f;
+    float currentVoltage = getVoltage(A0);
     if(currentVoltage > VOLTAGE_THRESHOLD && voltage <= VOLTAGE_THRESHOLD) {
         Serial.println("Door is closed");
         voltage = currentVoltage;
@@ -64,20 +64,26 @@ void postIsOpen(bool isOpen) {
     Serial.print("Posting isOpen: ");
     Serial.println(isOpen);
 
-    IPAddress addr;
-//    WiFi.hostByName(DOMAIN, addr);
-//    Serial.print("IP address: ");
-//    Serial.println(addr);
-//    if(client.connect(DOMAIN, 80)) {
-//        Serial.println("Connected to server");
-//        Serial.print("POST /garage-door/1?isOpen=");
-//        Serial.print(isOpen ? 1 : 0);
-//        Serial.println(" HTTP/1.1");
-//        Serial.print("Host: ");
-//        Serial.println(DOMAIN);
-//        Serial.print("Authorization: Basic ");
-//        Serial.println(AUTHORIZATION_HEADER);
-//        Serial.println("Connection: close");
-//        Serial.println();
-//    }
+    if(client.connect(DOMAIN1, 8888)) {
+        Serial.println("Connected to server");
+        Serial.print("POST /garage-door/1?isOpen=");
+        Serial.print(isOpen ? 1 : 0);
+        Serial.println(" HTTP/1.1");
+        Serial.print("Host: ");
+        Serial.println(DOMAIN);
+        Serial.print("Authorization: Basic ");
+        Serial.println(AUTHORIZATION_HEADER);
+        Serial.println("Connection: close");
+        Serial.println();
+
+        client.print("POST /garage-door/1?isOpen=");
+        client.print(isOpen ? 1 : 0);
+        client.println(" HTTP/1.1");
+        client.print("Host: ");
+        client.println(DOMAIN);
+        client.print("Authorization: Basic ");
+        client.println(AUTHORIZATION_HEADER);
+        client.println("Connection: close");
+        client.println();
+    }
 }
