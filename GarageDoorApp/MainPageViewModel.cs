67e996b48e9ac7c696114fefd98038af6893fceb
Kevin Morris <kmorris511@gmail.com>
2023-09-21 12:45:11 -0600

    Adding refresh button and activity indicator

--- GarageDoorApp/MainPageViewModel.cs
+++ GarageDoorApp/MainPageViewModel.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using GarageDoorApp.Api;
 using GarageDoorApp.Model;
 
@@ -17,6 +19,12 @@ namespace GarageDoorApp
         private readonly IGarageDoorApi _api;
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool IsBusy { get; private set; } = true;
+
+        public bool RefreshEnabled => !IsBusy;
+
+        public Command FetchStatusesCommand => new(FetchStatuses);
 
         private IEnumerable<GarageDoorStatus> _garageDoorStatuses;
         public IEnumerable<GarageDoorStatus> GarageDoorStatuses
@@ -32,7 +40,16 @@ namespace GarageDoorApp
 
         public async void FetchStatuses()
         {
-            GarageDoorStatuses = new List<GarageDoorStatus>(await _api.GetAsync());
+            IsBusy = true;
+            OnPropertyChanged(nameof(IsBusy));
+            OnPropertyChanged(nameof(RefreshEnabled));
+
+            var garageDoorStatuses = new List<GarageDoorStatus>(await _api.GetAsync());
+            GarageDoorStatuses = garageDoorStatuses;
+
+            IsBusy = false;
+            OnPropertyChanged(nameof(IsBusy));
+            OnPropertyChanged(nameof(RefreshEnabled));
         }
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
